4821d19558f6aa8307733d12ffc3f9a98d26ddaf
Your Name <you@example.com>
2026-03-03 16:28:43 -0300

    Update 0.1

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const app = require("./src/app")
 const PORT = process.env.PORT ||5000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)  
+  console.log(`Servidor rodando na porta ${PORT}`)
+app.get("/", (req, res) => {
+  res.send("Backend rodando no Render");
+});  
 })
